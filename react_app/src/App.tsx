15b03c78b571b3c05f31bfffca0b254bbab4e6b1
Gabryel <gabryelsoh@gmail.com>
2024-03-31 10:57:29 +0800

    using props to pass data to components dynamically. for example, same component for different data

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  // remember to wrap the items.map(item) in curly braces as its not html
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1); // -1 for no initial state?
   return (
     <>
-      <h1>List Group</h1>
+      <h1>{heading}</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item, index) => (
