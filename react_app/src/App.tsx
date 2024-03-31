fcd4b2eb1f10014fb7730ad0ba508af96d9e4097
Gabryel <gabryelsoh@gmail.com>
2024-03-31 11:10:07 +0800

    set up alert and removed listGroup

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,18 +1,10 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  // naming convention "handle -""
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert />
     </div>
   );
 }
