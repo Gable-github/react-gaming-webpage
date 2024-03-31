7392d4466ef92c51112568fe860d6334efd9283f
Gabryel <gabryelsoh@gmail.com>
2024-03-31 22:44:11 +0800

    updating array of objects without immer

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,6 +1,15 @@
-import Like from "./components/Like";
+import React, { useState } from "react";
+// import produce from 'immer';
 
 function App() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleOnClick = () => {
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+  };
   return (
     <div>
       <Like onClick={() => console.log("clicked")} />
