9aa8dfd3a9727b750ecc309efd98f5f50c747cf7
Gabryel <gabryelsoh@gmail.com>
2024-04-01 01:09:29 +0800

    added forms learned useRef

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi soluta
-        deserunt exercitationem beatae explicabo. Adipisci, et. Commodi
-        reprehenderit, optio possimus aspernatur eligendi molestiae repellendus
-        nisi adipisci veniam dolorem corrupti ipsam porro fugit ratione vel
-        molestias delectus voluptatem qui. Possimus odio, dolorum voluptatum
-        perspiciatis vero repudiandae! Possimus laboriosam quas, assumenda
-        deleniti id voluptate optio autem necessitatibus delectus aliquam earum
-        illum, dolore voluptatum mollitia, quo unde? Impedit enim modi excepturi
-        minima, qui, placeat deserunt dignissimos nulla dolore rerum distinctio
-        omnis unde saepe quos ducimus ad dolores? Voluptatum autem facere labore
-        esse, voluptatem et, libero enim sapiente quibusdam, adipisci nulla
-        quaerat ad quisquam.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
