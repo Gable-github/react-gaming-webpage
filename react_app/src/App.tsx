5ab910c7369797515aaab81c283536a8c3854709
Gabryel <gabryelsoh@gmail.com>
2024-04-01 00:12:54 +0800

    added expandable text component with functionality

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import produce from "immer";
-import { set } from "immer/dist/internal";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-
-  const handleOnClick = () => {
-    setCartItems([]);
-  };
-
   return (
     <div>
-      <Navbar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClick={handleOnClick} />
+      <ExpandableText>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi soluta
+        deserunt exercitationem beatae explicabo. Adipisci, et. Commodi
+        reprehenderit, optio possimus aspernatur eligendi molestiae repellendus
+        nisi adipisci veniam dolorem corrupti ipsam porro fugit ratione vel
+        molestias delectus voluptatem qui. Possimus odio, dolorum voluptatum
+        perspiciatis vero repudiandae! Possimus laboriosam quas, assumenda
+        deleniti id voluptate optio autem necessitatibus delectus aliquam earum
+        illum, dolore voluptatum mollitia, quo unde? Impedit enim modi excepturi
+        minima, qui, placeat deserunt dignissimos nulla dolore rerum distinctio
+        omnis unde saepe quos ducimus ad dolores? Voluptatum autem facere labore
+        esse, voluptatem et, libero enim sapiente quibusdam, adipisci nulla
+        quaerat ad quisquam.
+      </ExpandableText>
     </div>
   );
 }
