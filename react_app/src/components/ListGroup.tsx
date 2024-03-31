07516a8c0b545003c70b4b3d41adec7849c2f248
Gabryel <gabryelsoh@gmail.com>
2024-03-31 10:14:01 +0800

    map item, index functionality and handling on click li attribute. must use anon function in curly braces once again!

--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
 function ListGroup() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
   // remember to wrap the items.map(item) in curly braces as its not html
-  items = [];
+
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            onClick={() => console.log(item, index)}
+            key={item}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
