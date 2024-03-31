e80895a3090836a074102ad0fc4a112b8aef53ed
Gabryel <gabryelsoh@gmail.com>
2024-03-31 10:09:55 +0800

    using ternary operator - very common way to do conditional events rendering

--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
   // remember to wrap the items.map(item) in curly braces as its not html
+  items = [];
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
