bf78cc325852a54a14967e46d1a1874c8f6e22ca
Gabryel <gabryelsoh@gmail.com>
2024-03-31 10:20:41 +0800

    made mouse click event handler

--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,18 +1,16 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
   // remember to wrap the items.map(item) in curly braces as its not html
-
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            onClick={() => console.log(item, index)}
-            key={item}
-          >
+          <li className="list-group-item" onClick={handleClick} key={item}>
             {item}
           </li>
         ))}
