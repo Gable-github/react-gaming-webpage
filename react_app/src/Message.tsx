c2d8129ee427128f6743753f2c068c0b48d613fa
Gabryel <gabryelsoh@gmail.com>
2024-03-31 09:30:36 +0800

    first hello world message component

---
+++ react_app/src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    // this is Javascript XML
+    // Using Babeljs you can see how this "HTML" element really is
+    // implemented as one in javascript. JSX -> Javascript element -> HTML element
+    return <h1>Hello World!</h1>
+}
+
+export default Message;
