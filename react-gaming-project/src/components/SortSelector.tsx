981d59fd0d86e6b3a149f9ac6d81b3ca725554ba
Gabryel <gabryelsoh@gmail.com>
2024-04-07 15:35:06 +0800

    fix sorting issue

--- react-gaming-project/src/components/SortSelector.tsx
+++ react-gaming-project/src/components/SortSelector.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 const SortSelector = ({ onSortSelected, sortedOrder }: Props) => {
   const sortOrder = [
-    { value: "", label: "Relevance" },
-    { value: "-added", label: "Date added" },
+    { value: "-added", label: "Relevance" },
+    { value: "added", label: "Date added" },
     { value: "name", label: "Name" },
     { value: "-released", label: "Release date" },
-    { value: "metacritic", label: "Popularity" },
+    { value: "-metacritic", label: "Popularity" },
     { value: "-rating", label: "Average rating" },
   ];
 
